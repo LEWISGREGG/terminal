a0f036ce5ea367a32a25affdd2d9db8b35f953b2
SERG <40707060+LEWISGREGG@users.noreply.github.com>
2022-04-02 18:49:50 -0400

    Add files via upload

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -18,6 +18,7 @@ export default class Filter extends Component {
           <select value={this.props.size} onChange={this.props.filterProducts}>
             <option value="">ALL</option>
             <option value="25">25</option>
+            <option value="50">50</option>
             <option value="100">100</option>
             <option value="200">200</option>
             <option value="300">300</option>
